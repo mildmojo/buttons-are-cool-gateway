d99fb8f85ec8bc6ca9f85a41b42173588de514fe
mildmojo <mildmojo@users.noreply.github.com>
2017-09-23 01:09:40 -0400

    Adds --wrap-cmd.
    Use --wrap-cmd to tie the server's lifetime to a child process. This
    is useful as a wrapper to launch apps that depend on the server being
    accessible. Use --wrap-cwd to set the working directory for the
    subprocess.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const fs = require('fs');
+const {spawn} = require('child_process');
 const nconf = require('nconf');
 const cjson = require('cjson');
 const chalk = require('chalk');
@@ -19,15 +20,21 @@ if (nconf.get('version')) {
 
 if (nconf.get('help')) {
   console.log('Options:');
-  console.log('  --help        - Prints this usage info');
-  console.log('  --port n      - Listen port number for HTTP server (default: 3000)');
-  console.log("  --testmode    - Simulate serial port activity, don't open real ports");
-  console.log('  --version     - Prints server version number');
+  console.log('  --help          - Prints this usage info');
+  console.log('  --port n        - Listen port number for HTTP server (default: 3000)');
+  console.log("  --testmode      - Simulate serial port activity, don't open real ports");
+  console.log('  --wrap-cmd cmd  - Spawn `cmd` as a subprocess; kill server when subprocess dies and vice versa');
+  console.log('  --wrap-dir dir  - Working directory for wrapped command');
+  console.log('  --version       - Prints server version number');
   process.exit();
 }
 
 const server = require('./lib/server.js');
-server.start(nconf).catch(console.error);
+server
+  .start(nconf)
+  .then(() => console.log(chalk.blue.bold('Press CTRL+C to quit.')))
+  .then(() => nconf.get('wrap-cmd') && spawnChild())
+  .catch(console.error);
 
 function readConfig(file) {
   if (!fs.existsSync('./config.json')) {
@@ -45,4 +52,35 @@ function verifyConfig(config) {
     console.warn(`No serial ports configured! Please specify serial port(s) in ${configFile}.`);
     throw new Error('Serial ports not configured.');
   }
+}
+
+// Spawn a child process with a suicide pact; parent & child die together.
+function spawnChild() {
+  const cmd = nconf.get('wrap-cmd');
+  const cwd = nconf.get('wrap-dir');
+
+  console.log(chalk.green(`Spawning subprocess '${cmd}'...`))
+  const spawnOpts = {
+    stdio: 'inherit',
+    shell: true,
+    cwd: cwd
+  };
+  const child = spawn(cmd, [], spawnOpts);
+
+  function onChildExit(code) {
+    console.log(chalk.green('Wrapped process exited, server exiting...'));
+    process.removeListener('exit', onExit);
+    process.exit(code);
+  }
+  function onExit(code) {
+    console.log(chalk.green('Exiting, killing subprocess...'))
+    child.removeAllListeners();
+    process.removeListener('exit', onExit);
+    child.kill.bind(child);
+    process.exit(code);
+  }
+
+  process.on('SIGINT', onExit);
+  process.on('exit', onExit);
+  child.on('exit', onChildExit)
 }
