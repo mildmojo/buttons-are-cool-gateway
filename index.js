71221bb7217e1067437c96b773c229412a0092cd
mildmojo <mildmojo@users.noreply.github.com>
2017-09-23 01:44:51 -0400

    Adds --list to list available serial ports.
    Also exits when there's a serial port error.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const fs = require('fs');
 const {spawn} = require('child_process');
+const SerialPort = require('serialport');
 const nconf = require('nconf');
 const cjson = require('cjson');
 const chalk = require('chalk');
@@ -13,14 +14,13 @@ nconf.set('port', nconf.get('PORT') || nconf.get('port') || 3000)
 nconf.set('testmode', nconf.get('TESTMODE') || nconf.get('testmode'));
 verifyConfig(nconf);
 
-if (nconf.get('version')) {
-  console.log(version);
-  process.exit();
-}
+if (nconf.get('version')) showVersionQuit();
+if (nconf.get('list')) showPortsQuit();
 
 if (nconf.get('help')) {
   console.log('Options:');
   console.log('  --help          - Prints this usage info');
+  console.log('  --list          - Prints available serial ports');
   console.log('  --port n        - Listen port number for HTTP server (default: 3000)');
   console.log("  --testmode      - Simulate serial port activity, don't open real ports");
   console.log('  --wrap-cmd cmd  - Spawn `cmd` as a subprocess; kill server when subprocess dies and vice versa');
@@ -35,6 +35,26 @@ server
   .then(() => console.log(chalk.blue.bold('Press CTRL+C to quit.')))
   .then(() => nconf.get('wrap-cmd') && spawnChild())
   .catch(console.error);
+
+function showVersionQuit() {
+  console.log(version);
+  process.exit();
+}
+
+function showPortsQuit() {
+  SerialPort.list()
+    .then(portList => {
+      console.log(chalk.yellow.bold("Paste one or more of these serial ports into 'config.json':"));
+      for (let port of portList) {
+        let attrs = Object.keys(port)
+          .filter(key => key !== 'comName')
+          .reduce((sum, key) => port[key] ? sum.concat(`${key}: ${port[key]}`) : sum, []);
+        console.log(chalk.yellow(`"${port.comName}", ` + (attrs.length ? `// ${attrs.join(', ')}` : '')));
+      }
+      process.exit();
+    })
+    .catch(console.error);
+}
 
 function readConfig(file) {
   if (!fs.existsSync('./config.json')) {
